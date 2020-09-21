e099f6da9d85ccc5ab2cb9ccb642d6fbd91cc80f
Danielle Korman <dk@Danielles-MacBook-Pro.local>
2020-09-21 11:39:47 -0500

    Job class constructors added

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -1,4 +1,5 @@
 package org.launchcode.techjobs_oo;
+import java.util.Objects;
 
 public class Job {
 
@@ -15,6 +16,20 @@ public class Job {
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
 
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
+
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+        this();
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+
+    }
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
 
